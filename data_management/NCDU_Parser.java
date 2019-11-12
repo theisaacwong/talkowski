ff5315a8c3c05e2b9dee5cb791b69d240b18dea6
Isaac Wong <iwong@weocd.com>
2019-11-12 16:35:37 -0500

    Update NCDU_Parser.java

--- data_management/NCDU_Parser.java
+++ data_management/NCDU_Parser.java
@@ -27,8 +27,7 @@ import java.util.regex.Pattern;
 public class NCDU_Parser {
 
 	public static void main(String[] args) throws IOException {
-		
-		System.out.println(System.getProperty("java.version"));
+		System.out.println("Java version " + System.getProperty("java.version"));
 		if(args[0].equals("-h") || args[0].equals("--help")) {
 			System.out.println("java NCDU_Parser [ncdu_file] [output_file]");
 		} else {
@@ -58,6 +57,8 @@ public class NCDU_Parser {
 	
 	
 	public static void ncduParser(String INPUT_PATH, String OUTPUT_BASE) throws IOException {
+		long start = System.currentTimeMillis();
+		System.out.println("Reading file: ");
 
 		BufferedWriter output = null;
 		File file = new File(OUTPUT_BASE + ".tsv");
@@ -74,15 +75,16 @@ public class NCDU_Parser {
 
 		Stack<String> filePaths = new Stack<String>();
 
-		output.write("file\tasize\tdsize\thuman_readable\n");
+		output.write("file\tasize\tdsize\thuman_readable\tfile_extension\n");
 
-		HashMap<Integer, String> lineMap = new HashMap<Integer, String>();
-		HashMap<Long, ArrayList<Integer>> sortMap = new HashMap<Long, ArrayList<Integer>>();
-		int lineNumber = 1;
+		HashMap<Long, String> lineMap = new HashMap<Long, String>();
+		HashMap<Long, ArrayList<Long>> sortMap = new HashMap<Long, ArrayList<Long>>();
+		long lineNumber = 1;
 
 		Pattern ap = Pattern.compile("(?<=asize\":)\\d+");
 		Pattern dp = Pattern.compile("(?<=dsize\":)\\d+");
 		Pattern np = Pattern.compile("(?<=name\":\").+?(?=\")");
+		Pattern ep = Pattern.compile("(?<=\\.).*");
 		int count = 0;
 
 		while (sc.hasNextLine()) {
@@ -92,9 +94,9 @@ public class NCDU_Parser {
 			Matcher dm = dp.matcher(line); 
 			Matcher nm = np.matcher(line);  nm.find();
 
-			if(line.contains("[")) {
+			if(line.contains("[")) {	// new folder opening
 				filePaths.push(nm.group());
-				count = line.length() - line.replaceAll("]", "").length();
+				count = line.length() - line.replaceAll("]", "").length();	// folder(s) are closed
 				for(int i = 0; i < count; i++) {
 					if(filePaths.size() > 0)
 						filePaths.pop();
@@ -114,6 +116,12 @@ public class NCDU_Parser {
 			String asize = am.group(0);
 			String dsize = dm.group(0);
 			String name  = nm.group(0);
+			String fileExtension = "NA";
+			
+			Matcher em = ep.matcher(name);
+			if(em.find()) {
+				fileExtension = em.group(0);
+			} 
 			
 			StringBuilder sb = new StringBuilder();
 			for (Object s : filePaths.toArray()) {
@@ -127,6 +135,8 @@ public class NCDU_Parser {
 			sb.append(dsize);
 			sb.append("\t");
 			sb.append(humanReadableByteCount(asize));
+			sb.append("\t");
+			sb.append(fileExtension);
 			sb.append("\n");
 			String currPath = sb.toString();
 			output.write(currPath);
@@ -139,11 +149,12 @@ public class NCDU_Parser {
 			}
 
 			lineMap.put(lineNumber, currPath);
-			Long dSize = Long.parseLong(dsize);
-			if(sortMap.containsKey(dSize)) {
-				sortMap.get(dSize).add(lineNumber);
+			Long aSize = Long.parseLong(asize);
+			if(sortMap.containsKey(aSize)) {
+				sortMap.get(aSize).add(lineNumber);
 			} else {
-				sortMap.put(dSize, new ArrayList<Integer>());
+				sortMap.put(aSize, new ArrayList<Long>());
+				sortMap.get(aSize).add(lineNumber);
 			}
 			lineNumber++;
 		}
@@ -151,7 +162,10 @@ public class NCDU_Parser {
 		sc.close();
 		output.close();
 
-		System.out.println("now sorting");
+		long doneReading = System.currentTimeMillis();
+		System.out.println("done reading: ");
+		System.out.println("elapsed time: " + (doneReading - start) / 1000F + " seconds");
+		System.out.println("now sorting, n = " + sortMap.keySet().size());
 
 		output = null;
 		file = new File(OUTPUT_BASE + "_sorted.tsv");
@@ -161,14 +175,19 @@ public class NCDU_Parser {
 		ArrayList<Long> sortIndex = new ArrayList<>(sortMap.keySet());
 		Collections.sort(sortIndex, Collections.reverseOrder());
 
+		long doneSorting = System.currentTimeMillis();
+		System.out.println("elapsed time: " + (doneSorting - doneReading) / 1000F + " seconds");
 		System.out.println("done sorting, now writing");
 
 		for(Long si : sortIndex) {
-			for(Integer ln : sortMap.get(si)) {
+			for(Long ln : sortMap.get(si)) {
 				output.write(lineMap.get(ln));
 			}
 		}
 		output.close();
+		System.out.println("done writing");
+		long end = System.currentTimeMillis();
+		System.out.println("total elapsed time: " + (end - start) / 1000F + " seconds");
 
 	}
 
