2b11132495ea4231fdb26b8aef6b8875663f6c4b
Isaac Wong <iwong@weocd.com>
2020-01-10 09:50:16 -0500

    Update duplex_sequencing.sh

--- UMI/duplex_sequencing.sh
+++ UMI/duplex_sequencing.sh
@@ -64,25 +64,25 @@ echo "Step 3"; date
 # Step 3 FGBIO ExtractUmisFromBam
 STEP_03_OUTPUT=${TMP_DIR}${NAME}_step03_umi_extracted_unaligned.bam
 STEP_03_METRICS_01=${TMP_DIR}${NAME}_step03_est_lib_complex_metrics.txt
-java -Xmx${MEM} ${FGBIO_PATH} ExtractUmisFromBam --input=${STEP_02_OUTPUT} --output=${STEP_03_OUTPUT} --read-structure=${UMI1} ${UMI2} --molecular-index-tags=ZA ZB --single-tag=RX
-bsub "${BSUB_ARGS[@]}" java "${JAVA_ARGS[@]}" ${PICARD_PATH} EstimateLibraryComplexity -I ${STEP_03_OUTPUT} -O ${STEP_03_METRICS_01}
+java -Xmx${MEM} -jar "${FGBIO_PATH}" ExtractUmisFromBam --input=${STEP_02_OUTPUT} --output=${STEP_03_OUTPUT} --read-structure=${UMI1} ${UMI2} --molecular-index-tags=ZA ZB --single-tag=RX
+bsub "${BSUB_ARGS[@]}" java "${JAVA_ARGS[@]}" "${PICARD_PATH}" EstimateLibraryComplexity -I ${STEP_03_OUTPUT} -O ${STEP_03_METRICS_01}
 
 echo "Step 4"; date
 # Step 4 sort ubam by query name
 STEP_04_OUTPUT=${TMP_DIR}${NAME}_step04_queryname_sorted.ubam
-java "${JAVA_ARGS[@]}" ${PICARD_PATH} SortSam -I ${STEP_03_OUTPUT} -O ${STEP_04_OUTPUT} -SORT_ORDER queryname
+java "${JAVA_ARGS[@]}" "${PICARD_PATH}" SortSam -I ${STEP_03_OUTPUT} -O ${STEP_04_OUTPUT} -SORT_ORDER queryname
 
 echo "Step 5"; date
 # Step 5 Mark Illumina Adapters
 STEP_05_OUTPUT=${TMP_DIR}${NAME}_step05_markAdapt.ubam
-java "${JAVA_ARGS[@]}" ${PICARD_PATH} MarkIlluminaAdapters -I ${STEP_04_OUTPUT} -O ${STEP_05_OUTPUT} -M ${STEP_05_OUTPUT}_MarkAdaptMetrics.txt
+java "${JAVA_ARGS[@]}" "${PICARD_PATH}" MarkIlluminaAdapters -I ${STEP_04_OUTPUT} -O ${STEP_05_OUTPUT} -M ${STEP_05_OUTPUT}_MarkAdaptMetrics.txt
 
 echo "Step 6"; date
 # Step 6, Sam to fastq
 STEP_06_OUTPUT_01=${TMP_DIR}${NAME}_step06_R1.fastq
 STEP_06_OUTPUT_02=${TMP_DIR}${NAME}_step
 06_R2.fastq
-java "${JAVA_ARGS[@]}" ${PICARD_PATH} SamToFastq -I ${STEP_05_OUTPUT} -CLIPPING_ATTRIBUTE XT -CLIPPING_ACTION X -CLIPPING_MIN_LENGTH 36 -INCLUDE_NON_PF_READS true -F ${STEP_06_OUTPUT_01} -F2 ${STEP_06_OUTPUT_02}
+java "${JAVA_ARGS[@]}" "${PICARD_PATH}" SamToFastq -I ${STEP_05_OUTPUT} -CLIPPING_ATTRIBUTE XT -CLIPPING_ACTION X -CLIPPING_MIN_LENGTH 36 -INCLUDE_NON_PF_READS true -F ${STEP_06_OUTPUT_01} -F2 ${STEP_06_OUTPUT_02}
 
 echo "Step 7"; date
 # Step 7, map fastq 
@@ -98,31 +98,31 @@ STEP_08_METRICS_03=${TMP_DIR}${NAME}_step08_est_lib_complex_metrics.txt
 STEP_08_METRICS_04=${TMP_DIR}${NAME}_step08_insert_size_metrics.txt
 STEP_08_METRICS_05=${TMP_DIR}${NAME}_step08_insert_size_histogram.pdf
 STEP_08_METRICS_06=${TMP_DIR}${NAME}_step08_hs_metrics.txt
-java "${JAVA_ARGS[@]}" ${PICARD_PATH} MergeBamAlignment -UNMAPPED ${STEP_03_OUTPUT} -ALIGNED ${STEP_07_OUTPUT} -O ${STEP_08_OUTPUT} -R ${REF} -CLIP_ADAPTERS false -VALIDATION_STRINGENCY SILENT -CREATE_INDEX true -EXPECTED_ORIENTATIONS FR -MAX_GAPS -1 -SO coordinate -ALIGNER_PROPER_PAIR_FLAGS false
-bsub "${BSUB_ARGS[@]}" java "${JAVA_ARGS[@]}" ${PICARD_PATH} MarkDuplicates -I ${STEP_08_OUTPUT} -O ${STEP_08_METRICS_01} -M ${STEP_08_METRICS_02}
-bsub "${BSUB_ARGS[@]}" java "${JAVA_ARGS[@]}" ${PICARD_PATH} EstimateLibraryComplexity -I ${STEP_08_OUTPUT} -O ${STEP_08_METRICS_03}
-bsub "${BSUB_ARGS[@]}" java "${JAVA_ARGS[@]}" ${PICARD_PATH} CollectInsertSizeMetrics -I ${STEP_08_OUTPUT} -O ${STEP_08_METRICS_04} -H ${STEP_08_METRICS_05}
-bsub "${BSUB_ARGS[@]}" java "${JAVA_ARGS[@]}" ${PICARD_PATH} CollectHsMetrics -I ${STEP_08_OUTPUT} -O ${STEP_08_METRICS_06} -R ${REF} -BAIT_INTERVALS ${BAIT} -TARGET_INTERVALS ${BAIT}
+java "${JAVA_ARGS[@]}" "${PICARD_PATH}" MergeBamAlignment -UNMAPPED ${STEP_03_OUTPUT} -ALIGNED ${STEP_07_OUTPUT} -O ${STEP_08_OUTPUT} -R ${REF} -CLIP_ADAPTERS false -VALIDATION_STRINGENCY SILENT -CREATE_INDEX true -EXPECTED_ORIENTATIONS FR -MAX_GAPS -1 -SO coordinate -ALIGNER_PROPER_PAIR_FLAGS false
+bsub "${BSUB_ARGS[@]}" java "${JAVA_ARGS[@]}" "${PICARD_PATH}" MarkDuplicates -I ${STEP_08_OUTPUT} -O ${STEP_08_METRICS_01} -M ${STEP_08_METRICS_02}
+bsub "${BSUB_ARGS[@]}" java "${JAVA_ARGS[@]}" "${PICARD_PATH}" EstimateLibraryComplexity -I ${STEP_08_OUTPUT} -O ${STEP_08_METRICS_03}
+bsub "${BSUB_ARGS[@]}" java "${JAVA_ARGS[@]}" "${PICARD_PATH}" CollectInsertSizeMetrics -I ${STEP_08_OUTPUT} -O ${STEP_08_METRICS_04} -H ${STEP_08_METRICS_05}
+bsub "${BSUB_ARGS[@]}" java "${JAVA_ARGS[@]}" "${PICARD_PATH}" CollectHsMetrics -I ${STEP_08_OUTPUT} -O ${STEP_08_METRICS_06} -R ${REF} -BAIT_INTERVALS ${BAIT} -TARGET_INTERVALS ${BAIT}
 
 echo "Step 9"; date				# if you want a fastq files, after this step is the time to make them
 # Step 9, Group Reads by UMI
 STEP_09_OUTPUT=${TMP_DIR}${NAME}_step09_groupByUMI.bam
-java -Xmx${MEM} ${FGBIO_PATH} GroupReadsByUmi --strategy=paired --input=${STEP_08_OUTPUT} --output=${STEP_09_OUTPUT} --raw-tag=RX --assign-tag=MI --min-map-q=10 --edits=1
+java -Xmx${MEM} -jar "${FGBIO_PATH}" GroupReadsByUmi --strategy=paired --input=${STEP_08_OUTPUT} --output=${STEP_09_OUTPUT} --raw-tag=RX --assign-tag=MI --min-map-q=10 --edits=1
 
 echo "Step 10"; date
 # Step 10, Call duplex consensus reads
 #java -Djava.io.tmpdir=$TMP -Xmx$MEM -jar /data/talkowski/iwong/scripts/samToFastqKeepUMI_jre1.8.jar "${TMP_DIR}${NAME}_groupedUMI_step07.bam" "${TMP_DIR}${NAME}_groupedUMI_step07.5.fastq"
 STEP_10_OUTPUT=${TMP_DIR}${NAME}_step10_callDuplexConsensus.bam
 STEP_10_METRICS_01=${TMP_DIR}${NAME}_step10_est_lib_complex_metrics.txt
-java -Xmx${MEM} ${FGBIO_PATH} CallDuplexConsensusReads --input=${STEP_09_OUTPUT} --output=${STEP_10_OUTPUT} --error-rate-pre-umi=45 --error-rate-post-umi=30 --min-input-base-quality=10 --threads=${NT} --min-reads=0
-bsub "${BSUB_ARGS[@]}" java -Djava.io.tmpdir=${TMP} -Xmx${MEM} -jar ${PICARD_PATH} EstimateLibraryComplexity -I ${STEP_10_OUTPUT} -O ${STEP_10_METRICS_01}
+java -Xmx${MEM} -jar "${FGBIO_PATH}" CallDuplexConsensusReads --input=${STEP_09_OUTPUT} --output=${STEP_10_OUTPUT} --error-rate-pre-umi=45 --error-rate-post-umi=30 --min-input-base-quality=10 --threads=${NT} --min-reads=0
+bsub "${BSUB_ARGS[@]}" java "${JAVA_ARGS[@]}" "${PICARD_PATH}" EstimateLibraryComplexity -I ${STEP_10_OUTPUT} -O ${STEP_10_METRICS_01}
 
 echo "Step 11"; date
 # Step 11, remap duplex consensus reads
 STEP_11_OUTPUT_01=${TMP_DIR}${NAME}_step11_R1.fastq
 STEP_11_OUTPUT_02=${TMP_DIR}${NAME}_step11_R2.fastq
-java "${JAVA_ARGS[@]}" ${PICARD_PATH} SamToFastq VALIDATION_STRINGENCY=SILENT CREATE_INDEX=true -I ${STEP_10_OUTPUT} -F ${STEP_11_OUTPUT_01} -F2 ${STEP_11_OUTPUT_02} -INCLUDE_NON_PF_READS true
-bsub "${BSUB_ARGS[@]}" ${FASTQC} ${STEP_11_OUTPUT_01} ${STEP_11_OUTPUT_02}
+java "${JAVA_ARGS[@]}" "${PICARD_PATH}" SamToFastq VALIDATION_STRINGENCY=SILENT -CREATE_INDEX true -I ${STEP_10_OUTPUT} -F ${STEP_11_OUTPUT_01} -F2 ${STEP_11_OUTPUT_02} -INCLUDE_NON_PF_READS true
+bsub "${BSUB_ARGS[@]}" "${FASTQC}" ${STEP_11_OUTPUT_01} ${STEP_11_OUTPUT_02}
 
 echo "Step 12"; date
 # Step 12, map fastq 
@@ -138,32 +138,32 @@ STEP_13_METRICS_03=${TMP_DIR}${NAME}_step13_est_lib_complex_metrics.txt
 STEP_13_METRICS_04=${TMP_DIR}${NAME}_step13_insert_size_metrics.txt
 STEP_13_METRICS_05=${TMP_DIR}${NAME}_step13_insert_size_histogram.pdf
 STEP_13_METRICS_06=${TMP_DIR}${NAME}_step13_hs_metrics.txt
-java "${JAVA_ARGS[@]}" ${PICARD_PATH} MergeBamAlignment VALIDATION_STRINGENCY=SILENT CREATE_INDEX=true UNMAPPED=${STEP_10_OUTPUT} ALIGNED=${STEP_12_OUTPUT} OUTPUT=${STEP_13_OUTPUT} REFERENCE_SEQUENCE=${REF} CLIP_ADAPTERS=false ORIENTATIONS=FR MAX_GAPS=-1 SORT_ORDER=coordinate ALIGNER_PROPER_PAIR_FLAGS=false ATTRIBUTES_TO_RETAIN=X0 ATTRIBUTES_TO_RETAIN=ZS ATTRIBUTES_TO_RETAIN=ZI ATTRIBUTES_TO_RETAIN=ZM ATTRIBUTES_TO_RETAIN=ZC ATTRIBUTES_TO_RETAIN=ZN ATTRIBUTES_TO_REVERSE=ad ATTRIBUTES_TO_REVERSE=bd ATTRIBUTES_TO_REVERSE=cd ATTRIBUTES_TO_REVERSE=ae ATTRIBUTES_TO_REVERSE=be ATTRIBUTES_TO_REVERSE=ce
-bsub "${BSUB_ARGS[@]}" java "${JAVA_ARGS[@]}" ${PICARD_PATH} MarkDuplicates -I ${STEP_13_OUTPUT} -O ${STEP_13_METRICS_01} -M ${STEP_13_METRICS_02}
-bsub "${BSUB_ARGS[@]}" java "${JAVA_ARGS[@]}" ${PICARD_PATH} EstimateLibraryComplexity -I ${STEP_13_OUTPUT} -O ${STEP_13_METRICS_03}
-bsub "${BSUB_ARGS[@]}" java "${JAVA_ARGS[@]}" ${PICARD_PATH} CollectInsertSizeMetrics -I ${STEP_13_OUTPUT} -O ${STEP_13_METRICS_04} -H ${STEP_13_METRICS_05}
-bsub "${BSUB_ARGS[@]}" java "${JAVA_ARGS[@]}" ${PICARD_PATH} CollectHsMetrics -I ${STEP_13_OUTPUT} -O ${STEP_13_METRICS_06} -R ${REF} -BAIT_INTERVALS ${BAIT} -TARGET_INTERVALS ${BAIT}
+java "${JAVA_ARGS[@]}" ${PICARD_PATH} MergeBamAlignment -VALIDATION_STRINGENCY SILENT -CREATE_INDEX true -UNMAPPED ${STEP_10_OUTPUT} -ALIGNED ${STEP_12_OUTPUT} -OUTPUT ${STEP_13_OUTPUT} -REFERENCE_SEQUENCE ${REF} -CLIP_ADAPTERS false -ORIENTATIONS FR -MAX_GAPS -1 -SORT_ORDER coordinate -ALIGNER_PROPER_PAIR_FLAGS false -ATTRIBUTES_TO_RETAIN X0 -ATTRIBUTES_TO_RETAIN ZS -ATTRIBUTES_TO_RETAIN ZI -ATTRIBUTES_TO_RETAIN ZM -ATTRIBUTES_TO_RETAIN ZC -ATTRIBUTES_TO_RETAIN ZN -ATTRIBUTES_TO_REVERSE ad -ATTRIBUTES_TO_REVERSE bd -ATTRIBUTES_TO_REVERSE cd -ATTRIBUTES_TO_REVERSE ae -ATTRIBUTES_TO_REVERSE be -ATTRIBUTES_TO_REVERSE ce
+bsub "${BSUB_ARGS[@]}" java "${JAVA_ARGS[@]}" "${PICARD_PATH}" MarkDuplicates -I ${STEP_13_OUTPUT} -O ${STEP_13_METRICS_01} -M ${STEP_13_METRICS_02}
+bsub "${BSUB_ARGS[@]}" java "${JAVA_ARGS[@]}" "${PICARD_PATH}" EstimateLibraryComplexity -I ${STEP_13_OUTPUT} -O ${STEP_13_METRICS_03}
+bsub "${BSUB_ARGS[@]}" java "${JAVA_ARGS[@]}" "${PICARD_PATH}" CollectInsertSizeMetrics -I ${STEP_13_OUTPUT} -O ${STEP_13_METRICS_04} -H ${STEP_13_METRICS_05}
+bsub "${BSUB_ARGS[@]}" java "${JAVA_ARGS[@]}" "${PICARD_PATH}" CollectHsMetrics -I ${STEP_13_OUTPUT} -O ${STEP_13_METRICS_06} -R ${REF} -BAIT_INTERVALS ${BAIT} -TARGET_INTERVALS ${BAIT}
 
 echo "Step 14"; date
 # Step 14, FilterConsensusReads
 STEP_14_OUTPUT=${TMP_DIR}${NAME}_step14_filterConsensus.bam
 STEP_14_METRICS_01=${TMP_DIR}${NAME}_step14_hs_metrics.txt
-java -Xmx${MEM} -jar ${FGBIO_PATH} FilterConsensusReads -i ${STEP_13_OUTPUT} -o ${STEP_14_OUTPUT} --ref ${REF} --min-reads 10 5 3 --max-read-error-rate 0.05 --max-base-error-rate 0.1 --min-base-quality 50 --max-no-call-fraction 0.05
-bsub "${BSUB_ARGS[@]}" java "${JAVA_ARGS[@]}" ${PICARD_PATH} CollectHsMetrics -I ${STEP_14_OUTPUT} -O ${STEP_14_METRICS_01} -R ${REF} -BAIT_INTERVALS ${BAIT} -TARGET_INTERVALS ${BAIT}
+java -Xmx${MEM} -jar "${FGBIO_PATH}" FilterConsensusReads -i ${STEP_13_OUTPUT} -o ${STEP_14_OUTPUT} --ref ${REF} --min-reads 10 5 3 --max-read-error-rate 0.05 --max-base-error-rate 0.1 --min-base-quality 50 --max-no-call-fraction 0.05
+bsub "${BSUB_ARGS[@]}" java "${JAVA_ARGS[@]}" "${PICARD_PATH}" CollectHsMetrics -I ${STEP_14_OUTPUT} -O ${STEP_14_METRICS_01} -R ${REF} -BAIT_INTERVALS ${BAIT} -TARGET_INTERVALS ${BAIT}
 
 echo "Step 15"; date
 # Step 15, clip bam files 
 STEP_15_OUTPUT=${TMP_DIR}${NAME}_step15_clipBam.bam
 STEP_15_METRICS_01=${TMP_DIR}${NAME}_step15_est_lib_complex_metrics.txt
 STEP_15_METRICS_02=${TMP_DIR}${NAME}_step15_hs_metrics.txt
-java -Xmx${MEM} -jar ${FGBIO_PATH} ClipBam --input=${STEP_14_OUTPUT} --output=${STEP_15_OUTPUT} --ref=${REF} --clipping-mode=Hard --clip-overlapping-reads=true
-bsub "${BSUB_ARGS[@]}" java "${JAVA_ARGS[@]}" ${PICARD_PATH} EstimateLibraryComplexity -I ${STEP_15_OUTPUT} -O ${STEP_15_METRICS_01}
-bsub "${BSUB_ARGS[@]}" java "${JAVA_ARGS[@]}" ${PICARD_PATH} CollectHsMetrics -I ${STEP_15_OUTPUT} -O ${STEP_15_METRICS_02} -R ${REF} -BAIT_INTERVALS ${BAIT} -TARGET_INTERVALS ${BAIT}
+java -Xmx${MEM} -jar "${FGBIO_PATH}" ClipBam --input=${STEP_14_OUTPUT} --output=${STEP_15_OUTPUT} --ref=${REF} --clipping-mode=Hard --clip-overlapping-reads=true
+bsub "${BSUB_ARGS[@]}" java "${JAVA_ARGS[@]}" "${PICARD_PATH}" EstimateLibraryComplexity -I ${STEP_15_OUTPUT} -O ${STEP_15_METRICS_01}
+bsub "${BSUB_ARGS[@]}" java "${JAVA_ARGS[@]}" "${PICARD_PATH}" CollectHsMetrics -I ${STEP_15_OUTPUT} -O ${STEP_15_METRICS_02} -R ${REF} -BAIT_INTERVALS ${BAIT} -TARGET_INTERVALS ${BAIT}
 
 echo "Step 16"; date
 # Step 16, CollectDuplexSeqMetrics
 STEP_16_OUTPUT=${TMP_DIR}${NAME}_CollectDuplexSeqMetrics.txt
-java -Xmx${MEM} -jar ${FGBIO_PATH} CollectDuplexSeqMetrics --input=${STEP_09_OUTPUT} --output=${STEP_16_OUTPUT} --description=${NAME}
+java -Xmx${MEM} -jar "${FGBIO_PATH}" CollectDuplexSeqMetrics --input=${STEP_09_OUTPUT} --output=${STEP_16_OUTPUT} --description=${NAME}
 
 date
 
