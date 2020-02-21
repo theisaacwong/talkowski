f179ab3fc4a5d0167f17150ae72b2984196e2cc7
Isaac Wong <iwong@weocd.com>
2020-02-21 13:50:08 -0500

    Update duplex_sequencing.sh

--- UMI/duplex_sequencing.sh
+++ UMI/duplex_sequencing.sh
@@ -2,7 +2,7 @@
 
 # Isaac Wong
 # iwong1@mgh.harvard.edu
-# Version 1.11 January 21, 2020
+# Version 1.31 February 21, 2020
 # https://github.com/theisaacwong/
 
 date
@@ -28,21 +28,21 @@ NAME=$3
 TMP_DIR=$4
 TMP=$5
 MEM=$6
-UMI1="1S3M146T" 
-UMI2="1S3M146T"
+UMI1="3M2S+T" 
+UMI2="3M2S+T"
 GOTO_STEP=${7:-0}
 
 cd $TMP_DIR
 
-DELETE_INTERMEDIATE_FILES=false
-CONTINUE_ON=false
 
 NT=$(nproc)
 PARIFQ="/data/talkowski/iwong/software/pairfq_lite/pairfq_lite"
-REF="/data/talkowski/iwong/files/hg38/hg38.fa"
+REF="/data/talkowski/iwong/files/broad_references/Homo_sapiens_assembly38.fasta"
 PICARD_PATH="/data/talkowski/iwong/src/jars/picard.jar"
 FGBIO_PATH="/data/talkowski/iwong/software/fgbio/fgbio-1.0.0.jar"
+GATK_PATH="/data/talkowski/iwong/src/jars/gatk.jar"
 BAIT="/data/talkowski/iwong/files/intervals/Twist_Exome_Target_hg38.interval_list"
+GNOMAD="/data/talkowski/iwong/files/references/gnomad.genomes.r3.0.sites.vcf.bgz"
 
 LOG_DIR="${TMP_DIR}log_files/"
 mkdir "${LOG_DIR}"
@@ -72,6 +72,7 @@ STEP_12_OUTPUT=${TMP_DIR}${NAME}_step12_mapped.sam
 STEP_13_OUTPUT=${TMP_DIR}${NAME}_step13_merged.bam
 STEP_14_OUTPUT=${TMP_DIR}${NAME}_step14_filterConsensus.bam
 STEP_15_OUTPUT=${TMP_DIR}${NAME}_step15_clipBam.bam
+STEP_16_OUTPUT=${TMP_DIR}${NAME}_step16_HaplotypeCaller.output.g.vcf.gz
 
 #######################################
 ########### METRIC VARIABLES ##########
@@ -101,135 +102,103 @@ STEP_15_METRICS_04=${TMP_DIR}${NAME}_step15_CollectInsertSizeMetrics.pdf
 
 
 echo "Step 2"; date	
-if [ $GOTO_STEP -le 2 ] || [ $CONTINUE_ON == true ]; then
-CONTINUE_ON=true
+if [ $GOTO_STEP -le 2 ]; then
 # Step 2, convert R1 and R2 to ubam
-#READ_GROUP=$(head -1 ${FASTQ_PATH_R1} | awk -F ":" '{print $NF}')
 java "${JAVA_ARGS[@]}" ${PICARD_PATH} FastqToSam -O ${STEP_02_OUTPUT} -F1 ${FASTQ_PATH_R1} -F2 ${FASTQ_PATH_R2} -SM ${NAME} -LB Library1 -PU Unit1 -PL Illumina -RG ${READ_GROUP}
-bsub "${BSUB_ARGS[@]}" java "${JAVA_ARGS[@]}" ${PICARD_PATH} EstimateLibraryComplexity -I ${STEP_02_OUTPUT} -O ${STEP_02_METRICS_01}
 fi
 
 echo "Step 3"; date
-if [ $GOTO_STEP -le 3 ] || [ $CONTINUE_ON == true ]; then
-CONTINUE_ON=true
+if [ $GOTO_STEP -le 3 ]; then
 # Step 3 FGBIO ExtractUmisFromBam
-java -Xmx${MEM} -jar "${FGBIO_PATH}" ExtractUmisFromBam --input=${STEP_02_OUTPUT} --output=${STEP_03_OUTPUT} --read-structure=${UMI1} ${UMI2} --molecular-index-tags=ZA ZB --single-tag=RX
-bsub "${BSUB_ARGS[@]}" java "${JAVA_ARGS[@]}" "${PICARD_PATH}" EstimateLibraryComplexity -I ${STEP_03_OUTPUT} -O ${STEP_03_METRICS_01}
-[ $DELETE_INTERMEDIATE_FILES == true ] && [ -f $STEP_02_METRICS_01 ] && [ -f $STEP_03_OUTPUT ] && rm -rf $STEP_02_OUTPUT
+java -Xmx${MEM} -Djava.io.tmpdir=${TMP} -jar "${FGBIO_PATH}" ExtractUmisFromBam --input=${STEP_02_OUTPUT} --output=${STEP_03_OUTPUT} --read-structure=${UMI1} ${UMI2} --molecular-index-tags=ZA ZB --single-tag=RX
 fi
 
 echo "Step 4"; date
-if [ $GOTO_STEP -le 4 ] || [ $CONTINUE_ON == true ]; then
-CONTINUE_ON=true
+if [ $GOTO_STEP -le 4 ]; then
 # Step 4 sort ubam by query name
 java "${JAVA_ARGS[@]}" "${PICARD_PATH}" SortSam -I ${STEP_03_OUTPUT} -O ${STEP_04_OUTPUT} -SORT_ORDER queryname
 fi
 
 echo "Step 5"; date
-if [ $GOTO_STEP -le 5 ] || [ $CONTINUE_ON == true ]; then
-CONTINUE_ON=true
+if [ $GOTO_STEP -le 5 ]; then
 # Step 5 Mark Illumina Adapters
 java "${JAVA_ARGS[@]}" "${PICARD_PATH}" MarkIlluminaAdapters -I ${STEP_04_OUTPUT} -O ${STEP_05_OUTPUT} -M ${STEP_05_OUTPUT}_MarkAdaptMetrics.txt
-[ $DELETE_INTERMEDIATE_FILES == true ] && [ -f $STEP_05_OUTPUT ] && rm -rf $STEP_04_OUTPUT
 fi
 
 echo "Step 6"; date
-if [ $GOTO_STEP -le 6 ] || [ $CONTINUE_ON == true ]; then
-CONTINUE_ON=true
+if [ $GOTO_STEP -le 6 ]; then
 # Step 6, Sam to fastq
 java "${JAVA_ARGS[@]}" "${PICARD_PATH}" SamToFastq -I ${STEP_05_OUTPUT} -CLIPPING_ATTRIBUTE XT -CLIPPING_ACTION X -CLIPPING_MIN_LENGTH 36 -INCLUDE_NON_PF_READS true -F ${STEP_06_OUTPUT_01} -F2 ${STEP_06_OUTPUT_02}
-[ $DELETE_INTERMEDIATE_FILES == true ] && [ -f $STEP_06_OUTPUT_01 ] && [ -f $STEP_06_OUTPUT_02 ] && rm -rf $STEP_05_OUTPUT
 fi
 
 echo "Step 7"; date
-if [ $GOTO_STEP -le 7 ] || [ $CONTINUE_ON == true ]; then
-CONTINUE_ON=true
+if [ $GOTO_STEP -le 7 ]; then
 # Step 7, map fastq 
-bwa mem -t ${NT} ${REF} ${STEP_06_OUTPUT_01} ${STEP_06_OUTPUT_02} > ${STEP_07_OUTPUT}
-[ $DELETE_INTERMEDIATE_FILES == true ] && [ -f $STEP_07_OUTPUT ] && rm -rf $STEP_06_OUTPUT_01
-[ $DELETE_INTERMEDIATE_FILES == true ] && [ -f $STEP_07_OUTPUT ] && rm -rf $STEP_06_OUTPUT_02
+bwa mem -C -t ${NT} ${REF} ${STEP_06_OUTPUT_01} ${STEP_06_OUTPUT_02} > ${STEP_07_OUTPUT}
 fi
 
 echo "Step 8"; date		
-if [ $GOTO_STEP -le 8 ] || [ $CONTINUE_ON == true ]; then
+if [ $GOTO_STEP -le 8 ]; then
 CONTINUE_ON=true	
 # Step 8, merge bam alignment
 java "${JAVA_ARGS[@]}" "${PICARD_PATH}" MergeBamAlignment -UNMAPPED ${STEP_03_OUTPUT} -ALIGNED ${STEP_07_OUTPUT} -O ${STEP_08_OUTPUT} -R ${REF} -CLIP_ADAPTERS false -VALIDATION_STRINGENCY SILENT -CREATE_INDEX true -EXPECTED_ORIENTATIONS FR -MAX_GAPS -1 -SO coordinate -ALIGNER_PROPER_PAIR_FLAGS false
-bsub "${BSUB_ARGS[@]}" java "${JAVA_ARGS[@]}" "${PICARD_PATH}" MarkDuplicates -I ${STEP_08_OUTPUT} -O ${STEP_08_METRICS_01} -M ${STEP_08_METRICS_02}
-bsub "${BSUB_ARGS[@]}" java "${JAVA_ARGS[@]}" "${PICARD_PATH}" EstimateLibraryComplexity -I ${STEP_08_OUTPUT} -O ${STEP_08_METRICS_03}
-bsub "${BSUB_ARGS[@]}" java "${JAVA_ARGS[@]}" "${PICARD_PATH}" CollectInsertSizeMetrics -I ${STEP_08_OUTPUT} -O ${STEP_08_METRICS_04} -H ${STEP_08_METRICS_05}
-bsub "${BSUB_ARGS[@]}" java "${JAVA_ARGS[@]}" "${PICARD_PATH}" CollectHsMetrics -I ${STEP_08_OUTPUT} -O ${STEP_08_METRICS_06} -R ${REF} -BAIT_INTERVALS ${BAIT} -TARGET_INTERVALS ${BAIT}
-[ $DELETE_INTERMEDIATE_FILES == true ] && [ -f $STEP_08_OUTPUT ] && rm -rf $STEP_07_OUTPUT
 fi
 
 
 echo "Step 9"; date				# if you want a fastq files, after this step is the time to make them
-if [ $GOTO_STEP -le 9 ] || [ $CONTINUE_ON == true ]; then
-CONTINUE_ON=true
+if [ $GOTO_STEP -le 9 ]; then
 # Step 9, Group Reads by UMI
-java -Xmx${MEM} -jar "${FGBIO_PATH}" GroupReadsByUmi --strategy=paired --input=${STEP_08_OUTPUT} --output=${STEP_09_OUTPUT} --raw-tag=RX --assign-tag=MI --min-map-q=10 --edits=1
-# [ $DELETE_INTERMEDIATE_FILES == true ] && [ -f $STEP_08_METRICS_01 ] && [ -f $STEP_08_METRICS_02 ] && [ -f $STEP_08_METRICS_03 ] && [ -f $STEP_08_METRICS_04 ] && [ -f $STEP_08_METRICS_05 ] && [ -f $STEP_08_METRICS_06 ] && rm -rf $STEP_08_OUTPUT
-java -Xmx${MEM} -jar "${FGBIO_PATH}" CollectDuplexSeqMetrics --input=${STEP_09_OUTPUT} --output=${STEP_16_OUTPUT} --description=${NAME}
+java -Xmx${MEM} -Djava.io.tmpdir=${TMP} -jar "${FGBIO_PATH}" GroupReadsByUmi --strategy=paired --input=${STEP_08_OUTPUT} --output=${STEP_09_OUTPUT} --raw-tag=RX --assign-tag=MI --min-map-q=10 --edits=1
 fi
 
 echo "Step 10"; date
-if [ $GOTO_STEP -le 10 ] || [ $CONTINUE_ON == true ]; then
-CONTINUE_ON=true
+if [ $GOTO_STEP -le 10 ]; then
 # Step 10, Call duplex consensus reads
 #java -Djava.io.tmpdir=$TMP -Xmx$MEM -jar /data/talkowski/iwong/scripts/samToFastqKeepUMI_jre1.8.jar "${TMP_DIR}${NAME}_groupedUMI_step07.bam" "${TMP_DIR}${NAME}_groupedUMI_step07.5.fastq"
-java -Xmx${MEM} -jar "${FGBIO_PATH}" CallDuplexConsensusReads --input=${STEP_09_OUTPUT} --output=${STEP_10_OUTPUT} --error-rate-pre-umi=45 --error-rate-post-umi=30 --min-input-base-quality=10 --threads=${NT} --min-reads=0
-bsub "${BSUB_ARGS[@]}" java "${JAVA_ARGS[@]}" "${PICARD_PATH}" EstimateLibraryComplexity -I ${STEP_10_OUTPUT} -O ${STEP_10_METRICS_01}
-[ $DELETE_INTERMEDIATE_FILES == true ] && [ -f $STEP_10_OUTPUT ] && rm -rf $STEP_09_OUTPUT
+java -Xmx${MEM} -Djava.io.tmpdir=${TMP} -jar "${FGBIO_PATH}" CallDuplexConsensusReads --input=${STEP_09_OUTPUT} --output=${STEP_10_OUTPUT} --error-rate-pre-umi=45 --error-rate-post-umi=30 --min-input-base-quality=10 --threads=${NT} --min-reads=0
 fi
 
 echo "Step 11"; date
-if [ $GOTO_STEP -le 11 ] || [ $CONTINUE_ON == true ]; then
-CONTINUE_ON=true
+if [ $GOTO_STEP -le 11 ]; then
 # Step 11, remap duplex consensus reads
 java "${JAVA_ARGS[@]}" "${PICARD_PATH}" SamToFastq -VALIDATION_STRINGENCY SILENT -CREATE_INDEX true -I ${STEP_10_OUTPUT} -F ${STEP_11_OUTPUT_01} -F2 ${STEP_11_OUTPUT_02} -INCLUDE_NON_PF_READS true
-bsub "${BSUB_ARGS[@]}" "${FASTQC}" ${STEP_11_OUTPUT_01} ${STEP_11_OUTPUT_02}
-# [ $DELETE_INTERMEDIATE_FILES == true ] && [ -f $STEP_10_METRICS_01 ] && rm -rf $STEP_10_OUTPUT
 fi
 
 echo "Step 12"; date
-if [ $GOTO_STEP -le 12 ] || [ $CONTINUE_ON == true ]; then
-CONTINUE_ON=true
+if [ $GOTO_STEP -le 12 ]; then
 # Step 12, map fastq 
-bwa mem -t ${NT} ${REF} ${STEP_11_OUTPUT_01} ${STEP_11_OUTPUT_02} > ${STEP_12_OUTPUT}
-[ $DELETE_INTERMEDIATE_FILES == true ] && [ -f $STEP_12_OUTPUT ] && rm -rf $STEP_11_OUTPUT_01
-[ $DELETE_INTERMEDIATE_FILES == true ] && [ -f $STEP_12_OUTPUT ] && rm -rf $STEP_11_OUTPUT_02
+bwa mem -C -t ${NT} ${REF} ${STEP_11_OUTPUT_01} ${STEP_11_OUTPUT_02} > ${STEP_12_OUTPUT}
 fi
 
 echo "Step 13"; date
-if [ $GOTO_STEP -le 13 ] || [ $CONTINUE_ON == true ]; then
-CONTINUE_ON=true
+if [ $GOTO_STEP -le 13 ]; then
 # Step 13, merge bam alignment
 java "${JAVA_ARGS[@]}" ${PICARD_PATH} MergeBamAlignment -VALIDATION_STRINGENCY SILENT -CREATE_INDEX true -UNMAPPED ${STEP_10_OUTPUT} -ALIGNED ${STEP_12_OUTPUT} -OUTPUT ${STEP_13_OUTPUT} -REFERENCE_SEQUENCE ${REF} -CLIP_ADAPTERS false -ORIENTATIONS FR -MAX_GAPS -1 -SORT_ORDER coordinate -ALIGNER_PROPER_PAIR_FLAGS false -ATTRIBUTES_TO_RETAIN X0 -ATTRIBUTES_TO_RETAIN ZS -ATTRIBUTES_TO_RETAIN ZI -ATTRIBUTES_TO_RETAIN ZM -ATTRIBUTES_TO_RETAIN ZC -ATTRIBUTES_TO_RETAIN ZN -ATTRIBUTES_TO_REVERSE ad -ATTRIBUTES_TO_REVERSE bd -ATTRIBUTES_TO_REVERSE cd -ATTRIBUTES_TO_REVERSE ae -ATTRIBUTES_TO_REVERSE be -ATTRIBUTES_TO_REVERSE ce
-bsub "${BSUB_ARGS[@]}" java "${JAVA_ARGS[@]}" "${PICARD_PATH}" MarkDuplicates -I ${STEP_13_OUTPUT} -O ${STEP_13_METRICS_01} -M ${STEP_13_METRICS_02}
-bsub "${BSUB_ARGS[@]}" java "${JAVA_ARGS[@]}" "${PICARD_PATH}" EstimateLibraryComplexity -I ${STEP_13_OUTPUT} -O ${STEP_13_METRICS_03}
-bsub "${BSUB_ARGS[@]}" java "${JAVA_ARGS[@]}" "${PICARD_PATH}" CollectInsertSizeMetrics -I ${STEP_13_OUTPUT} -O ${STEP_13_METRICS_04} -H ${STEP_13_METRICS_05}
-bsub "${BSUB_ARGS[@]}" java "${JAVA_ARGS[@]}" "${PICARD_PATH}" CollectHsMetrics -I ${STEP_13_OUTPUT} -O ${STEP_13_METRICS_06} -R ${REF} -BAIT_INTERVALS ${BAIT} -TARGET_INTERVALS ${BAIT}
-[ $DELETE_INTERMEDIATE_FILES == true ] && [ -f $STEP_13_OUTPUT ] && rm -rf $STEP_12_OUTPUT
 fi
 
 
 echo "Step 14"; date
-if [ $GOTO_STEP -le 14 ] || [ $CONTINUE_ON == true ]; then
-CONTINUE_ON=true
+if [ $GOTO_STEP -le 14 ]; then
 # Step 14, FilterConsensusReads
-java -Xmx${MEM} -jar "${FGBIO_PATH}" FilterConsensusReads -i ${STEP_13_OUTPUT} -o ${STEP_14_OUTPUT} --ref ${REF} --min-reads 0 0 0 --max-read-error-rate 0.35 --max-base-error-rate 0.3 --min-base-quality 40 --max-no-call-fraction 0.25
-bsub "${BSUB_ARGS[@]}" java "${JAVA_ARGS[@]}" "${PICARD_PATH}" CollectHsMetrics -I ${STEP_14_OUTPUT} -O ${STEP_14_METRICS_01} -R ${REF} -BAIT_INTERVALS ${BAIT} -TARGET_INTERVALS ${BAIT}
+java -Xmx${MEM} -Djava.io.tmpdir=${TMP} -jar "${FGBIO_PATH}" FilterConsensusReads -i ${STEP_13_OUTPUT} -o ${STEP_14_OUTPUT} --ref ${REF} --min-reads 0 0 0 --max-read-error-rate 0.35 --max-base-error-rate 0.3 --min-base-quality 40 --max-no-call-fraction 0.25
 fi
 
 echo "Step 15"; date
-if [ $GOTO_STEP -le 15 ] || [ $CONTINUE_ON == true ]; then
-CONTINUE_ON=true
+if [ $GOTO_STEP -le 15 ]; then
 # Step 15, clip bam files # will need an alt file with no clipping of overlapping-reads for variant calling
-java -Xmx${MEM} -jar "${FGBIO_PATH}" ClipBam --input=${STEP_14_OUTPUT} --output=${STEP_15_OUTPUT} --ref=${REF} --clipping-mode=Hard --clip-overlapping-reads=true
+java -Xmx${MEM} -Djava.io.tmpdir=${TMP} -jar "${FGBIO_PATH}" ClipBam --input=${STEP_14_OUTPUT} --output=${STEP_15_OUTPUT} --ref=${REF} --clipping-mode=Hard --clip-overlapping-reads=true
 bsub "${BSUB_ARGS[@]}" java "${JAVA_ARGS[@]}" "${PICARD_PATH}" EstimateLibraryComplexity -I ${STEP_15_OUTPUT} -O ${STEP_15_METRICS_01}
 bsub "${BSUB_ARGS[@]}" java "${JAVA_ARGS[@]}" "${PICARD_PATH}" CollectHsMetrics -I ${STEP_15_OUTPUT} -O ${STEP_15_METRICS_02} -R ${REF} -BAIT_INTERVALS ${BAIT} -TARGET_INTERVALS ${BAIT}
 bsub "${BSUB_ARGS[@]}" java "${JAVA_ARGS[@]}" "${PICARD_PATH}" CollectInsertSizeMetrics -I ${STEP_15_OUTPUT} -O ${STEP_15_METRICS_03} -H ${STEP_15_METRICS_04}
 fi
 date
+
+echo "Step 16"; date
+if [ $GOTO_STEP -le 16 ]; then
+# Step 15, clip bam files # will need an alt file with no clipping of overlapping-reads for variant calling
+java -Xmx${MEM} -Djava.io.tmpdir=${TMP} -jar "${GATK_PATH}" HaplotypeCaller -R ${REF} -I ${STEP_15_OUTPUT} -O ${STEP_16_OUTPUT} -ERC GVCF --dbsnp ${GNOMAD} -L ${BAIT}
+fi
+date
+
 
 
